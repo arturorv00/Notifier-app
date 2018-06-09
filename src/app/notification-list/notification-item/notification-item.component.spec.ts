18082096e36ff67d7097cb9cbea7b2e305060b2d
aramirez <arturor@thehangar.cr>
2018-06-08 20:06:59 -0600

    Add unit testing

--- src/app/notification-list/notification-item/notification-item.component.spec.ts
+++ src/app/notification-list/notification-item/notification-item.component.spec.ts
@@ -1,25 +1,38 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from '../../app.component';
+import { NotificationListComponent } from '../../notification-list/notification-list.component';
+import { NotificationItemComponent } from '../../notification-list/notification-item/notification-item.component';
+import { NotificationListReducers } from '../../notification-list/store/notification-list.reducers';
+import { TimeAgoPipe } from 'time-ago-pipe';
+import { RefreshComponent } from '../../refresh/refresh.component';
 
-import { NotificationItemComponent } from './notification-item.component';
-
-describe('NotificationItemComponent', () => {
-  let component: NotificationItemComponent;
-  let fixture: ComponentFixture<NotificationItemComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ NotificationItemComponent ]
-    })
-    .compileComponents();
-  }));
-
+describe('RefreshComponent', () => {
   beforeEach(() => {
-    fixture = TestBed.createComponent(NotificationItemComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent,
+        NotificationListComponent,
+        NotificationItemComponent,
+        TimeAgoPipe,
+        RefreshComponent
+      ],
+      imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        StoreModule.forRoot({ notificationList: NotificationListReducers })
+      ],
+      providers: []
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create the Notification-Item Component', () => {
+    const fixture = TestBed.createComponent(NotificationItemComponent);
+    const componentIns = fixture.debugElement.componentInstance;
+    expect(componentIns).toBeTruthy();
   });
 });
--- src/app/notification-list/notification-list.component.spec.ts
+++ src/app/notification-list/notification-list.component.spec.ts
@@ -1,25 +1,45 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed, async } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppComponent } from '../app.component';
+import { NotificationListComponent } from '../notification-list/notification-list.component';
+import { NotificationItemComponent } from '../notification-list/notification-item/notification-item.component';
+import { NotificationListReducers } from '../notification-list/store/notification-list.reducers';
+import { TimeAgoPipe } from 'time-ago-pipe';
+import { RefreshComponent } from '../refresh/refresh.component';
 
-import { NotificationListComponent } from './notification-list.component';
-
-describe('NotificationListComponent', () => {
-  let component: NotificationListComponent;
-  let fixture: ComponentFixture<NotificationListComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ NotificationListComponent ]
-    })
-    .compileComponents();
-  }));
-
+describe('RefreshComponent', () => {
   beforeEach(() => {
-    fixture = TestBed.createComponent(NotificationListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent,
+        NotificationListComponent,
+        NotificationItemComponent,
+        TimeAgoPipe,
+        RefreshComponent
+      ],
+      imports: [
+        BrowserModule,
+        FormsModule,
+        ReactiveFormsModule,
+        StoreModule.forRoot({ notificationList: NotificationListReducers })
+      ],
+      providers: []
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create the Notification-List Component', () => {
+    const fixture = TestBed.createComponent(NotificationListComponent);
+    const componentIns = fixture.debugElement.componentInstance;
+    expect(componentIns).toBeTruthy();
+  });
+
+  it('should have 2 notifications', () => {
+    const fixture = TestBed.createComponent(NotificationListComponent);
+    const compiled = fixture.debugElement.nativeElement;
+    fixture.detectChanges();
+    expect(compiled.querySelectorAll('.list-group-item').length).toBe(2);
   });
 });
